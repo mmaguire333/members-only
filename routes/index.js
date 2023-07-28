ad9dde9b9daf5383661d65f218d384ea7c345105
Matt <mmaguire333@gmail.com>
2023-07-27 20:00:34 -0600

    Add the get and post requests for user sign up to the index router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const LocalStrategey = require('passport-local').Strategy
+
+const userController = require('../controllers/userController');
+const expressAsyncHandler = require('express-async-handler');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Members Only' });
 });
+
+router.get('/sign-up', userController.user_create_get);
+
+router.post('/sign-up', userController.user_create_post);
 
 module.exports = router;
